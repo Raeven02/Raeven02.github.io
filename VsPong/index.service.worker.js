dae64006af7213108c04fd3cba06398ce6128feb
Raeven02 <otlycann@gmail.com>
2026-02-16 23:24:01 -0600

    keyboard fix attempt

--- VsPong/index.service.worker.js
+++ VsPong/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771305511|276478886';
+const CACHE_VERSION = '1771305818|583070269';
 /** @type {string} */
 const CACHE_PREFIX = 'vs Pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
