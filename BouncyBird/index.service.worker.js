8e89b7ba41e13ceb7a6c05435c11cbf4765a613d
Raeven02 <otlycann@gmail.com>
2026-02-18 20:18:57 -0600

    Bug Fixes

--- BouncyBird/index.service.worker.js
+++ BouncyBird/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771466593|26507973';
+const CACHE_VERSION = '1771467468|190990485';
 /** @type {string} */
 const CACHE_PREFIX = 'BouncyBird-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
